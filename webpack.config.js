27c9509d19a66bbef3b7096383c4b2f325c115b9
kor <kor@dwarshuis.com>
2024-07-10 20:28:14 +0200

    Search and scraping functionality, “forked” (copied) from WOT-terms

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    // entry: './src/typesenseInstantSearchInit.js', // Your main JavaScript file
+    entry: {
+        main: './src/typesenseInstantSearchInit.js', // Your existing main JavaScript file
+        additional1: './src/typesenseInstantSearch.js', // Path to your additional JS file
+        // additional2: './src/typesenseHandleSearchModal.js', // Path to another additional JS file
+    },
+    output: {
+        filename: '[name].bundle.js', // Generates main.bundle.js, additional1.bundle.js, etc.
+        path: path.resolve(__dirname, 'dist'), // Directory for the bundled file
+    },
+    devServer: {
+        static: './dist', // Folder to serve files from
+        hot: true, // Enable hot module replacement
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // Matches any CSS file
+                // use: ['style-loader', 'css-loader'], // Processes CSS files
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'index.html', to: './' }, // Adjust the 'from' path as necessary
+                { from: 'indexed-in-KERISSE.html', to: './' }, // Adjust the 'from' path as necessary
+                { from: 'src/custom.css', to: './custom.css' },
+
+            ]
+        })
+        // ,
+        // new MiniCssExtractPlugin({
+        //     filename: '[name].css',
+        // })
+    ],
+};
