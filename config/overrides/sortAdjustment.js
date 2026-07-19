8f55c4b7b58ccb4c3a2dded941e1bfd117f60492
Kor Dwarshuis <proximus@fastmail.com>
2026-07-19 19:31:35 +0200

    Remove deprecated configuration and override scripts
    - Deleted `config/configOverrides.json` and `config/overrides/sortAdjustment.js` as they are no longer in use.
    - Updated `.gitignore` to remove the entry for `config/overrides/overridesID.json`.
    - Removed references to overrides in `scraper/main.sh` and deleted `scraper/overrides.sh` to streamline the import process.
    
    Signed-off-by: Kor Dwarshuis <proximus@fastmail.com>

--- config/overrides/sortAdjustment.js
+++
@@ -1,8 +0,0 @@
-export const queriesWithSortAdjustment = [
-    // example:
-    {
-        queryString: "keri",
-        sortAdjustment: 1,
-        urlSubstring: "https://github.com/WebOfTrust/keripy/"
-    }
-];
